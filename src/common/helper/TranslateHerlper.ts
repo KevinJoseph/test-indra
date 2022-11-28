73230c98f3111ee2285fba22d64c49d09ffe1c7e
KevinJoseph <kevramos95@gmail.com>
2022-11-28 11:39:48 -0500

    common: add translate helper

---
+++ src/common/helper/TranslateHerlper.ts
@@ -0,0 +1,17 @@
+import { GenericObject } from '../types/GenericObject';
+export function translate<M extends GenericObject>(schema: GenericObject, map: M): GenericObject {
+  let schemaValue: any;
+  let mapKey: keyof M;
+  let mappedKey: string;
+  const mapKeys = Object.keys(map);
+  const translatedObject: GenericObject = {};
+
+  for (mapKey of mapKeys) {
+    schemaValue = schema[mapKey as string];
+    mappedKey = map[mapKey] as string;
+
+    translatedObject[mappedKey] = schemaValue;
+  }
+
+  return translatedObject;
+}
